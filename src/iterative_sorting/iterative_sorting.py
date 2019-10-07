2997cbb182833473e3f883cc17c5b45e7a78f523
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-07 13:30:06 -0700

    find next smallest element after current index

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,8 +6,11 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-
+        for j in range(cur_index + 1, len(arr) - 1):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
+        
 
     return arr
 
