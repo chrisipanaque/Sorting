743563afbac0c1835c5b20f2eefee827887d6ed7
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-07 14:26:18 -0700

    bubble sort 4th attempt - passes all tests

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,13 +21,14 @@ def selection_sort(arr):
 def bubble_sort(arr):
     done = True
     while done == True:
+        done = False
         for i in range(0, len(arr) - 1):
             if arr[i] > arr[i + 1]:
                 temp = arr[i]
                 arr[i] = arr[i + 1]
                 arr[i + 1] = temp
-            else:
-                done = False
+                done = True
+
     return arr
 
 
