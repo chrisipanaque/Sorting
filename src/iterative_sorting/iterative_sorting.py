9120b2070303f13ea05d454848d497c3c312212d
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-07 14:06:19 -0700

    bubble sort third attempt

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -23,7 +23,9 @@ def bubble_sort(arr):
     while done == True:
         for i in range(0, len(arr) - 1):
             if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
             else:
                 done = False
     return arr
