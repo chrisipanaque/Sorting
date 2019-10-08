822e02c0e1117a157485c9d9dba4b57b73d45bf7
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-10-08 15:52:05 -0700

    code works after help from Daniel Starling

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,52 +1,80 @@
 # TO-DO: complete the help function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    # elements = len(arrA) + len(arrB)
-    # merged_arr = [0] * elements
-    merged_arr = []
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
     # TO-DO
-    while len(arrA) > 0 and len(arrB) > 0:
-        if arrA[0] < arrB[0]:
-            merged_arr.append(arrA.pop(0))
+    # while len(arrA) > 0 and len(arrB) > 0:
+    #     if arrA[0] < arrB[0]:
+    #         merged_arr.append(arrA.pop(0))
+    #     else:
+    #         merged_arr.append(arrB.pop(0))
+
+    # return merged_arr + arrA + arrB
+    m = l = r = 0
+    while l < len(arrA) and r < len(arrB):
+        if arrA[l] <= arrB[r]:
+            merged_arr[m] = arrA[l]
+            l += 1
         else:
-            merged_arr.append(arrB.pop(0))
+            merged_arr[m] = arrB[r]
+            r += 1
+        m += 1
 
-    return merged_arr + arrA + arrB
+    while l < len(arrA):
+        merged_arr[m] = arrA[l]
+        l += 1
+        m += 1
+
+    while r < len(arrB):
+        merged_arr[m] = arrB[r]
+        r += 1
+        m += 1
+
+    return merged_arr
 
 
-list1 = [-3, 9]
-list2 = [1, 8]
+# list1 = [-3, 9]
+# list2 = [1, 8]
 
-print(merge(list1, list2))
+# print("MERGED", merge(list1, list2))
 
-arr = [4, 8, 9, 1]
+# arr = [4, 8, 9, 1]
+# print(merge_sort(arr))
 
-center = len(arr) // 2
-left = arr[0:center]
-right = arr[center:]
+# center = len(arr) // 2
+# left = arr[0:center]
+# right = arr[center:]
 
-print(left)
-print(right)
+# print(left)
+# print(right)
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-        # TO-DO
-        # split array by 2
-        # left array 0 to center
-        # right array center to end
-        # return recursion merge(left, right)
+    # if len(arr) == 1:
+    #     return arr
 
-    if len(arr) == 1:
-        return arr
+    if len(arr) > 1:
+        center = len(arr) // 2
+        left = arr[:center]
+        right = arr[center:]
+        result_left = merge_sort(left)
+        result_right = merge_sort(right)
+        # print(result_left)
+        # print(result_right)
 
-    center = len(arr) // 2
-    left = arr[:center]
-    right = arr[center:]
+        arr = merge(result_left, result_right)
+    return arr
 
-    return merge(merge_sort(left), merge_sort(right))
 
+print("MERGESORT", merge_sort([3, 4, 0, 8, -3, 1, 5]))
+
+arr = [4, 8, 9, 1]
+print(merge_sort(arr))
 
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
